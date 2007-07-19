aleax/xorg

commit ccb9da383be42f87a29e8694d846f839ac05925a

drop privileges on alternate config file given with -xf86config

debian/local/xserver-wrapper.c
@@ -323,7 +323,7 @@ main(int argc, char **argv)
     }
 
     for (i = 1; i < argc; i++) {
-      if (!strcmp(argv[i], "-config")) {
+      if (!strcmp(argv[i], "-config" || !strcmp(argv[i], "-xf86config")) {
         if (setuid(getuid())) {
           perror("X unable to drop setuid privileges for alternate config");
           exit(1);

commit ade85effa21f9c9f353c587823046ec1b1a86c7f

Fix xserver-wrapper compile error.
Commit ccb9da383be42f87a29e8694d846f839ac05925a introduced a syntax error.

debian/local/xserver-wrapper.c
@@ -323,7 +323,7 @@ main(int argc, char **argv)
     }
 
     for (i = 1; i < argc; i++) {
-      if (!strcmp(argv[i], "-config" || !strcmp(argv[i], "-xf86config")) {
+      if (!strcmp(argv[i], "-config") || !strcmp(argv[i], "-xf86config")) {
         if (setuid(getuid())) {
           perror("X unable to drop setuid privileges for alternate config");
           exit(1);