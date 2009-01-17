d652421b5a8092a789fcef07177a8a344dbd72b3
Bryce Harrington <bryce@bryceharrington.org>
2009-01-17 03:43:41 -0800

    Adding system environment items recommended at http://intellinuxgraphics.org/how_to_report_bug.html
    Add lshal
    
    Add debug test code

--- debian/apport/source_xorg.py
+++ debian/apport/source_xorg.py
@@ -12,10 +12,53 @@ option) any later version.  See http://www.gnu.org/copyleft/gpl.html for
 the full text of the license.
 '''
 
+# TODO:
+#  - Create some general purpose routines (see source_network-manager.py)
+#  - Parse files to generate system_environment more concisely
+#  - Trim lshal output to just required info
+
 import os.path
 import subprocess
 
+def installed_version(pkg):
+    script = subprocess.Popen(['apt-cache', 'policy', pkg], stdout=subprocess.PIPE)
+    output = script.communicate()[0]
+    return output.split('\n')[1].replace("Installed: ", "")
+
 def add_info(report):
+    # Build System Environment
+    report['system']      = " distro:             Ubuntu\n"
+    try:
+        script = subprocess.Popen(['uname', '-m'], stdout=subprocess.PIPE)
+        report['system'] += " architecture:       " + script.communicate()[0]
+    except OSError:
+        pass
+    try:
+        script = subprocess.Popen(['uname', '-r'], stdout=subprocess.PIPE)
+        report['system'] += " kernel:             " + script.communicate()[0]
+    except OSError:
+        pass
+    try:
+        report['system'] += " xserver-xorg:     " + installed_version('xserver-xorg') + "\n"
+    except OSError:
+        pass
+    try:
+        report['system'] += " mesa:             " + installed_version('libgl1-mesa-glx') + "\n"
+    except OSError:
+        pass
+    try:
+        report['system'] += " libdrm:           " + installed_version('libdrm2') + "\n"
+    except OSError:
+        pass
+    try:
+        report['system'] += " -intel:           " + installed_version('xserver-xorg-video-intel') + "\n"
+    except OSError:
+        pass
+    try:
+        report['system'] += " -ati:             " + installed_version('xserver-xorg-video-ati') + "\n"
+    except OSError:
+        pass
+
     try:
         report['XorgConf'] = open('/etc/X11/xorg.conf').read()
     except IOError:
@@ -39,6 +82,12 @@ def add_info(report):
     try:
         script = subprocess.Popen(['lspci', '-vvnn'], stdout=subprocess.PIPE)
         report['LsPci'] = script.communicate()[0]
+    except OSError:
+        pass
+
+    try:
+        script = subprocess.Popen(['lshal'], stdout=subprocess.PIPE)
+        report['LsHal'] = script.communicate()[0]
     except OSError:
         pass
 
@@ -84,4 +133,9 @@ def add_info(report):
     except OSError:
         pass
 
-
+## DEBUGING ##
+if __name__ == '__main__':
+    report = {}
+    add_info(report)
+    for key in report:
+        print '[%s]\n%s' % (key, report[key])
